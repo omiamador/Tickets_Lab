f4760a47ca9d14f8c63d6fd8236af7ffd6c3c89f
omiamador <omipibebb@gmail.com>
2026-05-25 10:31:00 -0600

    GO ALIVE
    UPLOADING FILES FOR NEW TICKETS SYSTEM LABORATORY

---
+++ user.js
@@ -0,0 +1,30 @@
+// user.js - Base de Datos de Usuarios y Roles HUF Lab
+const USERS_DATABASE = [
+    {
+        user: "1",
+        pass: "1",
+        role: "admin",
+        fullName: "Omar Amador"
+    },
+    {
+        user: "2",
+        pass: "2",
+        role: "user",
+        fullName: "Ignacio Rodriguez"
+    },
+    {
+        user: "3",
+        pass: "3",
+        role: "reader",
+        fullName: "Angela Lara"
+    },
+    {
+        user: "admin_Omi",
+        pass: "1234",
+        role: "admin",
+        fullName: "Omar Amador"
+    }
+];
+
+// Mantenemos tu lista fija de personal para asignaciones de tickets
+const LISTA_PERSONAL = ["Omar Amador", "Ignacio Rodriguez", "Antonio Bonilla", "Alejandro Lopez"];
